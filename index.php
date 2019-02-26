34585604c94c5ac9ecf9fec77a2d84d6104944ec
Sheyla <1530140@upv.edu.mx>
2019-02-26 01:18:57 -0600

    Commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+	<title></title>
+</head>
+<body>
+	<div class="body"></div>
+		<div class="grad"></div>
+		<div class="header">
+			<div>Site<span>Random</span></div>
+		</div>
+		<br>
+		<div class="login">
+				<input type="text" placeholder="username" name="user"><br>
+				<input type="password" placeholder="password" name="password"><br>
+				<a href="presupuesto.php"><input type="button" value="Login"></a>
+		</div>
+</body>
+</html>
+
